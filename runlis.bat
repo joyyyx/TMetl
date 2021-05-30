7933b4a9aedcd13955d46b866f5c5e1c60f39d1d
joyyyx <814875272@qq.com>
2021-05-30 17:29:19 +0800

    upfile

---
+++ runlis.bat
@@ -0,0 +1,3 @@
+echo sid: 3 > sid.yaml
+CD D:/data-integration  
+kitchen  /file:D:/TMetl/main.kjb 
---
+++ runpacs.bat
@@ -0,0 +1,3 @@
+echo sid: 4 > sid.yaml
+CD D:/data-integration  
+kitchen  /file:D:/TMetl/main.kjb 
